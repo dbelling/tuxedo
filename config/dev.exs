72d96a4c5d016b36108f558f96f64a6e52428f55
Dan Belling <danhbelling@gmail.com>
2016-02-03 11:20:28 -0600

    Configuring node and webpack for development

--- config/dev.exs
+++ config/dev.exs
@@ -12,7 +12,9 @@ config :tuxedo, Tuxedo.Endpoint,
   code_reloader: true,
   cache_static_lookup: false,
   check_origin: false,
-  watchers: []
+  watchers: [
+    node: ["node_modules/webpack/bin/webpack.js", "--watch", "--color"]
+  ]
 
 # Watch static and templates for browser reloading.
 config :tuxedo, Tuxedo.Endpoint,
