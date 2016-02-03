c205feacccb6ca32d9a6bff87069a96a270d8373
Dan Belling <danhbelling@gmail.com>
2016-02-03 12:01:02 -0600

    General routes and application layout

--- web/router.ex
+++ web/router.ex
@@ -9,18 +9,9 @@ defmodule Tuxedo.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", Tuxedo do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "*path", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Tuxedo do
-  #   pipe_through :api
-  # end
 end
